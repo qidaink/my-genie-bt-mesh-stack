64c5bd84bfdd81b1294db4c86ddd31ba3be65d9a
qidaink <2038035593@qq.com>
2023-12-31 16:02:34 +0800

    feat:从helloword example创建新的工程bt_node(未做任何修改)

---
+++ app/example/bluetooth/bt_node/1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# * =====================================================
+# * Copyright © hk. 2022-2025. All rights reserved.
+# * File name  : 1.sh
+# * Author     : 上上签
+# * Date       : 2023-12-24
+# * ======================================================
+##
+
+aos make bluetooth.bt_node@tc825x
